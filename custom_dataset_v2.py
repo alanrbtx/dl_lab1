f7c77205ceb2afa437026be3bff6fc300ad266b8
a.gazzaev <a.gazzaev@mts.ai>
2023-11-04 23:53:09 +0300

    custom_dataset_v2

---
+++ custom_dataset_v2.py
@@ -0,0 +1,40 @@
+import torch
+from torch.utils.data import Dataset
+from scipy.io import loadmat
+from skimage import io
+import os
+from PIL import Image
+
+class StanfordCars(Dataset):
+    def __init__(
+            self,
+            root_dir,
+            transforms=None,
+            train=True
+        ):
+
+        self.root = root_dir
+        self.transforms = transforms
+        self.train = train
+
+        if self.train:
+            self.annotation = loadmat("stanford_cars/devkit/cars_train_annos.mat", squeeze_me=True)["annotations"]
+            self.subdir = "cars_train"
+        else:
+            self.annotation = loadmat("stanford_cars/devkit/cars_test_annos.mat", squeeze_me=True)["annotations"]
+            self.subdir = "cars_test"
+    
+    def __len__(self):
+        return len(self.annotation)
+    
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.root, self.subdir, self.annotation[index]["fname"])
+        image = Image.open(img_path).convert("RGB")
+        label = torch.tensor(self.annotation[index]["class"])
+
+        if self.transforms:
+            image = self.transforms(image)
+
+        return image, label
+
